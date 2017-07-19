064736529e34b3697eae820db9b90d3495274a9b
InnovateBham_8 <innovatebham_8@UABiLAB-8s-iMac.local>
2017-07-19 18:02:49 -0500

    ...

--- client/app/app.js
+++ client/app/app.js
@@ -1,24 +1,37 @@
-var app = angular.module("myApp", ["ngRoute"]);
-app.config(function($routeProvider) {
+var mainApp = angular.module("mainApp", ["ngRoute", "controllers"]);
+mainApp.config(function($routeProvider) {
     $routeProvider
+    .when("/", {
+        templateUrl: '../views/home.html'
+    })
     .when("/list", {
-        templateUrl : "/views/list.html"
+        templateUrl : '../views/list.html',
+        controller : 'listController'
     })
     .when("/add", {
-        templateUrl : "/views/add.html"
+        templateUrl : '../views/add.html',
+        controller : 'addController'
     })
     .when("/single", {
-        templateUrl : "/views/single.html"
-    });
-    
-    app.controller('control', function() {
-
-    });
-
-    app.controller('myCtrl', function($scope, $http) {
-    $http.get("welcome.htm")
-    .then(function(response) {
-        $scope.myWelcome = response.data;
+        templateUrl : '../views/single.html',
+        controller : 'singleController'
     });
 });
-});
+
+ 
+    angular.module('controllers', [])
+    .controller('singleController', ['$scope', function() {
+        console.log('inside single controller')
+    }])
+    .controller('listController', ['$scope', function() {
+        console.log('inside list controller')
+    }])
+    .controller('addController', ['$scope', function() {
+        console.log('inside add controller')
+    }]);
+//     // // app.controller('myCtrl', function($scope, $http) {
+//     // // $http.get("welcome.htm")
+//     // // .then(function(response) {
+//     // //     $scope.myWelcome = response.data;
+//     // });
+// });
