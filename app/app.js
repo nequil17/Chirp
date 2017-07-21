99e3fc66b2f9173de4b3f5de2200bc98673b7c45
Nequil Hill <nequil17@hotmail.com>
2017-07-20 20:30:28 -0500

    Add files via upload

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
-var mainApp = angular.module("mainApp", ["ngRoute", "controllers"]);
+var mainApp = angular.module("mainApp", ["ngRoute", "controllers", ]);
+
 mainApp.config(function($routeProvider) {
     $routeProvider
     .when("/", {
@@ -12,7 +13,7 @@ mainApp.config(function($routeProvider) {
         templateUrl : '../views/add.html',
         controller : 'addController'
     })
-    .when("/single", {
+    .when("/single/:id", {
         templateUrl : '../views/single.html',
         controller : 'singleController'
     });
@@ -21,17 +22,64 @@ mainApp.config(function($routeProvider) {
  
     angular.module('controllers', [])
     .controller('singleController', ['$scope', function() {
-        console.log('inside single controller')
+        
     }])
     .controller('listController', ['$scope', function() {
-        console.log('inside list controller')
+        
     }])
     .controller('addController', ['$scope', function() {
-        console.log('inside add controller')
+        
     }]);
-//     // // app.controller('myCtrl', function($scope, $http) {
-//     // // $http.get("welcome.htm")
-//     // // .then(function(response) {
-//     // //     $scope.myWelcome = response.data;
-//     // });
-// });
+
+    mainApp.controller('getRequest', function($http, $scope, $location) {
+        $http.get('http://localhost:3000/api/chirps')
+        .then(function(response) { 
+            $scope.postList = response.data;
+        });
+        $scope.getId=function(id) {
+            $location.path('/single/' + id);
+        }
+        $scope.deleteData = function(id){
+       $http.delete("/api/chirps/" + id)
+           .success(function(response){
+               $http.get('/api/chirps')
+               .then(function (response) {
+               console.log(response);
+               $scope.postList = response.data;
+           });
+       });    
+   }
+    });
+
+    mainApp.controller('addController', function($scope, $http, $location) {
+        $scope.insertData = function() {
+          $http.post('/api/chirps', {'user' : $scope.user, 'message' : $scope.message})
+          .then(function(response) {
+            $scope.postList = response.data;
+            $scope.user = '';
+            $scope.message = '';  
+            });     
+        }  
+    });
+
+    mainApp.controller('singleController', function($scope, $routeParams, $http, $location) {
+        var currentId = $routeParams.id;
+        $http.get('http://localhost:3000/api/chirps/' + currentId)
+        .then(function(response) {
+            $scope.postList = response.data;
+        });
+    
+    
+    $scope.deleteData = function(id){
+       $http.delete("/api/chirps/" + id)
+           .success(function(response){
+               $http.get('/api/chirps')
+               .then(function (response) {
+               console.log(response);
+               $scope.postList = response.data;
+               $location.path('/list/');
+           });
+       });    
+   }
+
+        });
